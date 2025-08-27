fa07816990dcf3de38f8743a3bdf61ecd5bb78a4
Emmanuel Manzoor <codewithemmanuel@gmail.com>
2025-08-27 03:14:46 +0000

    see this blue background is when we scroll down in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { HearSayClient } from '@/components/hearsay-client';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center p-4">
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100 dark:from-gray-900 dark:via-gray-800 dark:to-blue-900 -z-10" />
+    <main className="flex flex-col items-center justify-center min-h-screen w-full p-4">
+      <div className="fixed top-0 left-0 w-full h-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100 dark:from-gray-900 dark:via-gray-800 dark:to-blue-900 -z-10" />
       <HearSayClient />
     </main>
   );
